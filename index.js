8b2cf16a32e30a72b9cce9fcdc2c40c303310fcd
Harald Kirschner <hkirschner@mozilla.com>
2014-08-15 11:45:35 -0700

    Made script arguments OS-specific (= for nix), closes #9. Switched from q to es6-promise for the greater good of standards. Version bump.

--- index.js
+++ index.js
@@ -7,12 +7,33 @@ var path = require('path');
 var fs = require('fs');
 var childProcess = require('child_process');
 var JSFtp = require('jsftp');
-var q = require('q');
+var Promise = require('es6-promise').Promise;
 var temp = require('temp');
 var mkdirp = require('mkdirp');
 var moment = require('moment');
 var yargs = require('yargs');
 var colors = require('colors');
+
+// Helper
+function promisify(fn) {
+	var args = Array.prototype.slice.call(arguments, 1);
+	return new Promise(function(resolve, reject) {
+		args.push(function(err) {
+			if (err != null) {
+				reject(err);
+			} else {
+				resolve.apply(null, Array.prototype.slice.call(arguments, 1));
+			}
+		});
+		if (Array.isArray(fn)) {
+			var scope = fn[0];
+			var method = fn[1];
+			scope[method].apply(scope, args);
+		} else {
+			fn.apply(null, args);
+		}
+	});
+}
 
 // Command-line arguments
 var yarg = yargs.usage('Shallow-flash Gecko and Gaia on Firefox OS devices from Mozilla\'s public build server (http://ftp.mozilla.org/pub/mozilla.org/b2g/nightly/).\nUsage: flash-b2g [device] [channel]')
@@ -90,22 +111,17 @@ if (!dir) {
 }
 
 if (argv['only-remotify']) {
-	setDeveloperPrefs().done(function() {
-		process.exit();
-	});
+	setDeveloperPrefs()
+		.then(function() {
+			console.log('✓ Done (only-remotify)');
+		}, function(err) {
+			console.error(err);
+		})
+		.then(function() {
+			process.exit();
+		});
 	return;
 }
-
-// FTP connection
-// TODO: Allow login
-var ftp = new JSFtp({
-	host: FTP_HOST
-});
-ftp.useList = true;
-ftp.socket.setTimeout(TIMEOUT);
-ftp.socket.once('timeout', function(data) {
-	ftp.socket.destroy(); // should call the "ls" callback "err"
-});
 
 var dateBit = (argv.date) ? argv.date.format('YYYY-MM-DD') : 'latest';
 var channelBit = argv.channel;
@@ -122,11 +138,8 @@ nameBits.push(dateBit);
 var pathMatch = new RegExp('^' + pathBits.join('-') + '$', 'i');
 var ftpPath = FTP_PATH;
 
-var b2gFile = null;
-var gaiaFile = null;
-
 // Strip the regex bits that are filesystem wildcards or otherwise weird out.
-var safeNameBits = nameBits.join('-').replace(/[^\w-]/gi, '');
+var safeNameBits = nameBits.join('-').replace(/[^\w_-]+/gi, '_');
 var localGaiaPath = path.join(dir, 'gaia-' + safeNameBits + '.zip');
 var localB2gPath = path.join(dir, 'b2g-' + safeNameBits + '.tar.gz');
 
@@ -150,39 +163,39 @@ function setDeveloperPrefs() {
 
 	// Wait for device
 	console.log('Waiting for device (is remote debugging on?)');
-	return q.nfcall(childProcess.exec, ADB + ' wait-for-device')
+	return promisify(childProcess.exec, ADB + ' wait-for-device')
 
 	// Stop B2G
-	.then(function() {
+	.then(function stopB2g() {
 		console.log('Stopping system');
-		return q.nfcall(childProcess.exec, [
+		return promisify(childProcess.exec, [
 			ADB + ' remount', // really needed?
 			ADB + ' shell stop b2g'
 		].join(' && '));
 	})
 
 	// Push preferences
-	.then(function() {
+	.then(function pushPreferences() {
 		var cmds = ['cd /data/b2g/mozilla/*.default/']
 			.concat(Object.keys(prefs).map(function(key) {
 				return 'echo \'user_pref(' + JSON.stringify(key) + ', ' +
 					JSON.stringify(prefs[key]) + ');\' >> prefs.js';
 			})).join(' && ');
 		console.log('Appending to prefs.js:\n', prefs);
-		return q.nfcall(childProcess.exec, ADB + ' shell "' +
+		return promisify(childProcess.exec, ADB + ' shell "' +
 			cmds.replace(/"/g, '\\"') + '"', {
 				maxBuffer: 524288
 			});
 	})
 
 	// Fetch settings.json
-	.then(function() {
-		return q.nfcall(childProcess.exec, ADB + ' shell cat /system/b2g/defaults/settings.json', {
-				maxBuffer: 524288
-			});
+	.then(function pullSettings() {
+		return promisify(childProcess.exec, ADB + ' shell cat /system/b2g/defaults/settings.json', {
+			maxBuffer: 524288
+		});
 	})
 
-	.spread(function(stdout) {
+	.then(function pushSettings(stdout) {
 		var content = JSON.parse(stdout);
 		for (var key in settings) {
 			content[key] = settings[key];
@@ -190,7 +203,7 @@ function setDeveloperPrefs() {
 		var settingsPath = path.join(tempDir, 'settings.json');
 		fs.writeFileSync(settingsPath, JSON.stringify(content));
 		console.log('Appending to settings.json:\n', settings);
-		return q.nfcall(childProcess.exec, [
+		return promisify(childProcess.exec, [
 			ADB + ' shell mount -o rw,remount /system',
 			ADB + ' push ' + settingsPath + ' /system/b2g/defaults/settings.json',
 			ADB + ' shell mount -o ro,remount /system'
@@ -198,38 +211,44 @@ function setDeveloperPrefs() {
 	})
 
 	// Restart B2G
-	.then(function() {
+	.then(function restartB2g() {
 		console.log('Restarting system');
-		return q.nfcall(childProcess.exec, ADB + ' shell sync && ' +
+		return promisify(childProcess.exec, ADB + ' shell sync && ' +
 			ADB + ' shell start b2g')
 	});
 };
 
-// Start process
-q.fcall(function() {
-	if (argv.local) {
-		console.log('Skipping FTP');
-		return [];
-	}
-	console.log('Connecting to %s', FTP_HOST.underline.white);
+function download() {
+	// FTP connection
+	// TODO: Allow login
+	var ftp = new JSFtp({
+		host: FTP_HOST
+	});
+	ftp.useList = true;
+	ftp.socket.setTimeout(TIMEOUT);
+	ftp.socket.once('timeout', function(data) {
+		ftp.socket.destroy(); // should call the "ls" callback "err"
+	});
 
+	console.log('Connecting to %s', FTP_HOST.underline.white);
 	// List files in main dir
-	return q.ninvoke(ftp, 'ls', ftpPath)
-		// Find target dir
-		.then(function findTargetFolder(files) {
-			files = files.filter(function(file) {
-				return pathMatch.test(file.name);
-			});
-			if (!files.length) {
-				throw new Error('Could not find dir matching ' + pathMatch);
-			}
-			return files[0].name;
-		})
+	return promisify([ftp, 'ls'], ftpPath)
+
+	// Find target dir
+	.then(function findTargetFolder(files) {
+		files = files.filter(function(file) {
+			return pathMatch.test(file.name);
+		});
+		if (!files.length) {
+			throw new Error('Could not find a directory matching ' + pathMatch + '.\nVerify your --device, --channel or --date argument and that a matching directory exists on the Mozilla FTP:\n' + FTP_URL + ftpPath);
+		}
+		return files[0].name;
+	})
 
 	// List files in target dir
 	.then(function listTargetFiles(path) {
 		ftpPath += path + '/';
-		return q.ninvoke(ftp, 'ls', ftpPath);
+		return promisify([ftp, 'ls'], ftpPath);
 	})
 
 	// Find b2g package
@@ -245,76 +264,102 @@ q.fcall(function() {
 
 	// Open sockets for b2g and gaia packages
 	.then(function openStreams(file) {
-		gaiaFile = file;
-		var deferGaia = q.defer();
-		http.get(FTP_URL + ftpPath + 'gaia.zip', deferGaia.resolve)
-			.on('error', deferGaia.reject);
-		var deferB2g = q.defer();
-		http.get(FTP_URL + ftpPath + gaiaFile, deferB2g.resolve)
-			.on('error', deferB2g.reject);
-		return [deferGaia.promise, deferB2g.promise];
+		return Promise.all([
+			new Promise(function(resolve, reject) {
+				http.get(FTP_URL + ftpPath + 'gaia.zip', resolve)
+					.on('error', reject);
+			}), new Promise(function(resolve, reject) {
+				http.get(FTP_URL + ftpPath + file, resolve)
+					.on('error', reject);
+			})
+		]);
 	})
 
 	// Stream socket data into files
-	.spread(function downloadStreams(gaiaStream, b2gStream) {
+	.then(function downloadStreams(streams) {
 		console.log('Downloading from %s', (FTP_URL + ftpPath).underline.white);
 		var fileFlags = {
 			flags: 'w'
 		};
-		var deferGaia = q.defer();
-		gaiaFile = fs.createWriteStream(localGaiaPath, fileFlags);
-		gaiaFile.on('finish', function() {
-			gaiaFile.close(deferGaia.resolve);
-		});
-		gaiaStream.pipe(gaiaFile);
-
-		var deferB2g = q.defer();
-		b2gFile = fs.createWriteStream(localB2gPath, {
-			flags: 'w'
-		});
-		b2gFile.on('finish', function() {
-			b2gFile.close(deferB2g.resolve);
-		});
-		b2gStream.pipe(b2gFile);
-		return [deferB2g.promise, deferGaia.promise];
+		return Promise.all([
+			new Promise(function(resolve, reject) {
+				var file = fs.createWriteStream(localGaiaPath, fileFlags);
+				file.on('finish', function() {
+					file.close(resolve);
+				});
+				streams[0].pipe(file);
+			}),
+			new Promise(function(resolve, reject) {
+				var file = fs.createWriteStream(localB2gPath, {
+					flags: 'w'
+				});
+				file.on('finish', function() {
+					file.close(resolve);
+				});
+				streams[1].pipe(file);
+			})
+		]);
 	});
+}
+
+// Start process
+Promise.resolve().then(function() {
+	if (argv.local) {
+		console.log('Skipping FTP');
+		return;
+	}
+	return download();
 })
 
-// Execute flash script
-.spread(function executeFlash() {
+.then(function() {
 	if (!fs.existsSync(localGaiaPath) || !fs.existsSync(localB2gPath)) {
 		throw new Error('Local files not found:\n' + localGaiaPath + '\n' + localB2gPath);
 	}
+})
 
-	var defer = q.defer();
+.then(function() {
+	console.log('Waiting for device (is remote debugging on?)');
+	return promisify(childProcess.exec, ADB + ' wait-for-device')
+})
+
+// Execute flash script
+.then(function executeFlash() {
+	var separator = (process.platform == 'linux') ? '=' : '';
 	// At least on linux bash, getopt defines that optional arguments need to be
 	// like "--opt=arg".  Only required arguments can do "--opt arg".
-	var args = ['-y', '--gaia=' + localGaiaPath, '--gecko=' + localB2gPath];
+	var args = [
+		'--gaia' + separator,
+		localGaiaPath,
+		'--gecko' + separator,
+		localB2gPath,
+		'-y'
+	];
 	if (argv.profile) {
 		console.log('Attempting to keep profile')
 		args.push('--keep_profile')
 	}
 	console.log('Executing ' + ('shallow_flash.sh ' + args.join(' ')).grey.italic);
-	var flash = childProcess.spawn(FLASH_SCRIPT_PATH, args, {
-		cwd: tempDir,
-		env: {
-			PROFILE_HOME: path.join(dir, 'b2g-profile-' + argv.device)
-		}
+	return new Promise(function(resolve, reject) {
+		var flash = childProcess.spawn(FLASH_SCRIPT_PATH, args, {
+			cwd: tempDir,
+			env: {
+				PROFILE_HOME: path.join(dir, 'b2g-profile-' + argv.device)
+			}
+		});
+		flash.stdout.on('data', function(data) {
+			console.log('[shallow_flash] '.grey + String(data).trim());
+		});
+		flash.stderr.on('data', function(data) {
+			console.log('[shallow_flash] '.red + String(data).trim());
+		});
+		flash.on('close', function(code) {
+			if (code) {
+				reject('Error code from shallow_flash.sh!');
+			} else {
+				resolve();
+			}
+		});
 	});
-	flash.stdout.on('data', function(data) {
-		console.log('[shallow_flash] '.grey + String(data).trim());
-	});
-	flash.stderr.on('data', function(data) {
-		console.log('[shallow_flash] '.red + String(data).trim());
-	});
-	flash.on('close', function(code) {
-		if (code) {
-			defer.reject('shallow_flash.sh failed!');
-		} else {
-			defer.resolve();
-		}
-	});
-	return defer.promise;
 })
 
 // Set developer prefs
@@ -325,9 +370,10 @@ q.fcall(function() {
 })
 
 // Listo!
-.done(function() {
-	console.log('✓ Done');
-}, function(err) {
-	console.error(String(err).red.bold);
+.then(function() {
+	console.log('✓ %s flashed to %s!'.bold.green, argv.device, argv.channel);
 	process.exit();
+}, function(err) {
+	console.error(String(err).bold.red);
+	process.exit(1);
 });
